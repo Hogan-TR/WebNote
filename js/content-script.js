2c6202fd1139918aea9320831b423bf80324c4d9
15262856393tr@gmail.com <15262856393tr@gmail.com>
2020-06-04 16:24:01 +0800

    select intermediate nodes with dfs

--- js/content-script.js
+++ js/content-script.js
@@ -1,4 +1,4 @@
-function selectText() {
+function gRange() {
     // save all nodes that between 'start'
     // and 'end' node(contain two endpoints)
     let selNodes = [];
@@ -11,35 +11,63 @@ function selectText() {
     // 'range' contain 'start' and 'end'
     // node of data that we have selected
     const range = sel.getRangeAt(0);
-    const start = {
-        node: range.startContainer,
-        offset: range.startOffset,
-        type: "startNode",
-    };
-    selNodes.push(split(start.node, start.offset, start.type));
-    const end = {
-        node: range.endContainer,
-        offset: range.endOffset,
-        type: "endNode",
-    };
-    selNodes.push(split(end.node, end.offset, end.type));
-
-    console.log(selNodes);
+    let resNodes = dfsNodes(range);
+    console.log(resNodes);
 }
 
-function split(node, offset, type) {
-    if (type === "startNode") {
-        if (node.nodeType === 3) {
-            node.splitText(offset);
-            return node.nextSibling;
-        }
-    } else if (type === "endNode") {
-        if (node.nodeType === 3) {
-            node.splitText(offset);
-            return node;
+document.onmouseup = () => {
+    gRange();
+};
+
+function dfsNodes(range) {
+    // extract data of S&E
+    const startNode = range.startContainer;
+    const startOffset = range.startOffset;
+    const endNode = range.endContainer;
+    const endOffset = range.endOffset;
+
+    // JUST one node
+    if (startNode === endNode) {
+        if (startNode.nodeType === 3) {
+            startNode.splitText(startOffset);
+            let nextNode = startNode.nextSibling;
+            nextNode.splitText(endOffset - startOffset);
+            return [nextNode];
         }
     }
+
+    let nodeList = []; // Stack Traversed
+    let resNodes = []; // List recored
+    let curNode = null;
+    let withS = false; // Tag that means whether record node to resNodes
+    let root = window.document; // Root node of the page
+    nodeList.push(root);
+    // Deep first
+    while ((curNode = nodeList.pop())) {
+        const children = curNode.childNodes;
+        for (let i = children.length - 1; i >= 0; i--) {
+            nodeList.push(children[i]);
+        }
+
+        if (curNode === startNode) {
+            if (curNode.nodeType === 3) {
+                curNode.splitText(startOffset);
+                resNodes.push(curNode.nextSibling);
+            }
+            // start record
+            console.log("start traverse");
+            withS = true;
+        } else if (curNode === endNode) {
+            if (curNode.nodeType === 3) {
+                curNode.splitText(endOffset);
+                resNodes.push(curNode);
+            }
+            // end record
+            console.log("end traverse");
+            break;
+        } else if (withS && curNode.nodeType === 3) {
+            resNodes.push(curNode);
+        }
+    }
+    return resNodes;
 }
-document.onmouseup = () => {
-    selectText();
-};
