b2a1122897d2b0554fce12c382ddf2d49d4e6708
Hogan-TR <15262856393tr@gmail.com>
2020-08-05 17:03:57 +0800

    change directory structure

---
+++ js/utils.js
@@ -0,0 +1,46 @@
+/* string formatting extension */
+String.prototype.replaceAll = function (exp, newStr) {
+    return this.replace(new RegExp(exp, "gm"), newStr);
+};
+String.prototype.format = function (args) {
+    var result = this;
+    if (arguments.length < 1) {
+        return result;
+    }
+
+    var data = arguments;
+    if (arguments.length == 1 && typeof args == "object") {
+        data = args;
+    }
+    for (var key in data) {
+        var value = data[key];
+        if (undefined != value) {
+            result = result.replaceAll("\\{" + key + "\\}", value);
+        }
+    }
+    return result;
+};
+
+/* generate unique id */
+function uuid(len, radix) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(
+        ""
+    );
+    var uuid = [],
+        i;
+    radix = radix || chars.length;
+    if (len) {
+        for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)];
+    } else {
+        var r;
+        uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
+        uuid[14] = "4";
+        for (i = 0; i < 36; i++) {
+            if (!uuid[i]) {
+                r = 0 | (Math.random() * 16);
+                uuid[i] = chars[i == 19 ? (r & 0x3) | 0x8 : r];
+            }
+        }
+    }
+    return uuid.join("");
+}
