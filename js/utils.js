dd3677b24ab39197ccdb7fe89ab67d33b2c36ea4
Jacob <trzerone@gmail.com>
2023-03-04 21:22:13 +0800

    feat(manifest): upgrade to v3 (#2)
    - upgrade manifest version
    - fix some problems about "auto close"

--- js/utils.js
+++ js/utils.js
@@ -1,24 +1,24 @@
 /* string formatting extension */
 String.prototype.replaceAll = function (exp, newStr) {
-    return this.replace(new RegExp(exp, "gm"), newStr);
+  return this.replace(new RegExp(exp, "gm"), newStr);
 };
 String.prototype.format = function (args) {
-    var result = this;
-    if (arguments.length < 1) {
-        return result;
-    }
-
-    var data = arguments;
-    if (arguments.length == 1 && typeof args == "object") {
-        data = args;
-    }
-    for (var key in data) {
-        var value = data[key];
-        if (undefined != value) {
-            result = result.replaceAll("\\{" + key + "\\}", value);
-        }
-    }
+  var result = this;
+  if (arguments.length < 1) {
     return result;
+  }
+
+  var data = arguments;
+  if (arguments.length == 1 && typeof args == "object") {
+    data = args;
+  }
+  for (var key in data) {
+    var value = data[key];
+    if (undefined != value) {
+      result = result.replaceAll("\\{" + key + "\\}", value);
+    }
+  }
+  return result;
 };
 
 /**
@@ -29,26 +29,25 @@ String.prototype.format = function (args) {
  * @returns {string} full id
  */
 function uuid(type, len, radix) {
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(
-        ""
-    );
-    var uuid = [],
-        i;
-    radix = radix || chars.length;
-    if (len) {
-        for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)];
-    } else {
-        var r;
-        uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
-        uuid[14] = "4";
-        for (i = 0; i < 36; i++) {
-            if (!uuid[i]) {
-                r = 0 | (Math.random() * 16);
-                uuid[i] = chars[i == 19 ? (r & 0x3) | 0x8 : r];
-            }
-        }
+  var chars =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
+  var uuid = [],
+    i;
+  radix = radix || chars.length;
+  if (len) {
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)];
+  } else {
+    var r;
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
+    uuid[14] = "4";
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | (Math.random() * 16);
+        uuid[i] = chars[i == 19 ? (r & 0x3) | 0x8 : r];
+      }
     }
-    return type + uuid.join("");
+  }
+  return type + uuid.join("");
 }
 
 /**
@@ -56,127 +55,127 @@ function uuid(type, len, radix) {
  * Reference link: https://segmentfault.com/a/1190000008187911
  */
 function deepCompare(x, y) {
-    var i, l, leftChain, rightChain;
+  var i, l, leftChain, rightChain;
 
-    function compare2Objects(x, y) {
-        var p;
+  function compare2Objects(x, y) {
+    var p;
 
-        // remember that NaN === NaN returns false
-        // and isNaN(undefined) returns true
-        if (
-            isNaN(x) &&
-            isNaN(y) &&
-            typeof x === "number" &&
-            typeof y === "number"
-        ) {
-            return true;
-        }
-
-        // Compare primitives and functions.
-        // Check if both arguments link to the same object.
-        // Especially useful on the step where we compare prototypes
-        if (x === y) {
-            return true;
-        }
-
-        // Works in case when functions are created in constructor.
-        // Comparing dates is a common scenario. Another built-ins?
-        // We can even handle functions passed across iframes
-        if (
-            (typeof x === "function" && typeof y === "function") ||
-            (x instanceof Date && y instanceof Date) ||
-            (x instanceof RegExp && y instanceof RegExp) ||
-            (x instanceof String && y instanceof String) ||
-            (x instanceof Number && y instanceof Number)
-        ) {
-            return x.toString() === y.toString();
-        }
-
-        // At last checking prototypes as good as we can
-        if (!(x instanceof Object && y instanceof Object)) {
-            return false;
-        }
-
-        if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
-            return false;
-        }
-
-        if (x.constructor !== y.constructor) {
-            return false;
-        }
-
-        if (x.prototype !== y.prototype) {
-            return false;
-        }
-
-        // Check for infinitive linking loops
-        if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
-            return false;
-        }
-
-        // Quick checking of one object being a subset of another.
-        // todo: cache the structure of arguments[0] for performance
-        for (p in y) {
-            if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
-                return false;
-            } else if (typeof y[p] !== typeof x[p]) {
-                return false;
-            }
-        }
-
-        for (p in x) {
-            if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
-                return false;
-            } else if (typeof y[p] !== typeof x[p]) {
-                return false;
-            }
-
-            switch (typeof x[p]) {
-                case "object":
-                case "function":
-                    leftChain.push(x);
-                    rightChain.push(y);
-
-                    if (!compare2Objects(x[p], y[p])) {
-                        return false;
-                    }
-
-                    leftChain.pop();
-                    rightChain.pop();
-                    break;
-
-                default:
-                    if (x[p] !== y[p]) {
-                        return false;
-                    }
-                    break;
-            }
-        }
-
-        return true;
+    // remember that NaN === NaN returns false
+    // and isNaN(undefined) returns true
+    if (
+      isNaN(x) &&
+      isNaN(y) &&
+      typeof x === "number" &&
+      typeof y === "number"
+    ) {
+      return true;
     }
 
-    if (arguments.length < 1) {
-        return true; //Die silently? Don't know how to handle such case, please help...
-        // throw "Need two or more arguments to compare";
+    // Compare primitives and functions.
+    // Check if both arguments link to the same object.
+    // Especially useful on the step where we compare prototypes
+    if (x === y) {
+      return true;
     }
 
-    for (i = 1, l = arguments.length; i < l; i++) {
-        leftChain = []; //Todo: this can be cached
-        rightChain = [];
+    // Works in case when functions are created in constructor.
+    // Comparing dates is a common scenario. Another built-ins?
+    // We can even handle functions passed across iframes
+    if (
+      (typeof x === "function" && typeof y === "function") ||
+      (x instanceof Date && y instanceof Date) ||
+      (x instanceof RegExp && y instanceof RegExp) ||
+      (x instanceof String && y instanceof String) ||
+      (x instanceof Number && y instanceof Number)
+    ) {
+      return x.toString() === y.toString();
+    }
 
-        if (!compare2Objects(arguments[0], arguments[i])) {
+    // At last checking prototypes as good as we can
+    if (!(x instanceof Object && y instanceof Object)) {
+      return false;
+    }
+
+    if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
+      return false;
+    }
+
+    if (x.constructor !== y.constructor) {
+      return false;
+    }
+
+    if (x.prototype !== y.prototype) {
+      return false;
+    }
+
+    // Check for infinitive linking loops
+    if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
+      return false;
+    }
+
+    // Quick checking of one object being a subset of another.
+    // todo: cache the structure of arguments[0] for performance
+    for (p in y) {
+      if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+        return false;
+      } else if (typeof y[p] !== typeof x[p]) {
+        return false;
+      }
+    }
+
+    for (p in x) {
+      if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+        return false;
+      } else if (typeof y[p] !== typeof x[p]) {
+        return false;
+      }
+
+      switch (typeof x[p]) {
+        case "object":
+        case "function":
+          leftChain.push(x);
+          rightChain.push(y);
+
+          if (!compare2Objects(x[p], y[p])) {
             return false;
-        }
+          }
+
+          leftChain.pop();
+          rightChain.pop();
+          break;
+
+        default:
+          if (x[p] !== y[p]) {
+            return false;
+          }
+          break;
+      }
     }
 
     return true;
+  }
+
+  if (arguments.length < 1) {
+    return true; //Die silently? Don't know how to handle such case, please help...
+    // throw "Need two or more arguments to compare";
+  }
+
+  for (i = 1, l = arguments.length; i < l; i++) {
+    leftChain = []; //Todo: this can be cached
+    rightChain = [];
+
+    if (!compare2Objects(arguments[0], arguments[i])) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
  * Returns the current timestamp
  */
 function getTimeNow() {
-    var now = new Date();
-    return now.getTime();
+  var now = new Date();
+  return now.getTime();
 }
