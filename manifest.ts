61e590055319ff0510338ef9beefa4b81169a137
Hogan-TR <trzerone@gmail.com>
2023-06-06 23:41:02 +0800

    feat: update vite build
    * make manifest
    * define build entry & change code struct

---
+++ manifest.ts
@@ -0,0 +1,18 @@
+import packageJson from './package.json'
+
+const manifest: chrome.runtime.ManifestV3 = {
+  manifest_version: 3,
+  name: packageJson.name,
+  version: packageJson.version,
+  description: packageJson.description,
+  content_scripts: [
+    {
+      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      js: ['src/pages/content/index.js'],
+      // KEY for cache invalidation
+      // css: ['assets/css/contentStyle<KEY>.chunk.css'],
+    },
+  ],
+}
+
+export default manifest
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,60 @@
-import { defineConfig } from 'vite'
-import { crx } from '@crxjs/vite-plugin'
+import { defineConfig, resolveEnvPrefix } from 'vite'
+import path, { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import zipPack from 'vite-plugin-zip-pack'
+import manifest from './manifest'
+import makeManifest from './utils/plugins/make-manifest'
+import customDynamicImport from './utils/plugins/custom-dynamic-import'
 
-import manifest from './src/manifest'
-//@ts-ignore
-import { config } from './src/read_pages_folder'
+const root = resolve(__dirname, 'src')
+const pagesDir = resolve(root, 'pages')
+const assetsDir = resolve(root, 'assets')
+const outDir = resolve(__dirname, 'build')
+const publicDir = resolve(__dirname, 'public')
 
-// https://vitejs.dev/config/
+const isDev = process.env.__DEV__ === 'true'
+const isProd = !isDev
+
 export default defineConfig(({ mode }) => {
   return {
+    publicDir,
     build: {
-      emptyOutDir: true,
-      outDir: 'build',
+      outDir,
+      // sourcemap: isDev,
+      minify: isProd,
+      reportCompressedSize: isProd,
       rollupOptions: {
-        input: config,
+        input: {
+          popup: resolve(pagesDir, 'popup', 'index.html'),
+          options: resolve(pagesDir, 'options', 'index.html'),
+          content: resolve(pagesDir, 'content', 'index.ts'),
+          background: resolve(pagesDir, 'background', 'index.ts'),
+        },
+        watch: {
+          include: ['src/**', 'vite.config.ts'],
+          exclude: ['node_modules/**', 'src/**/*.spec.ts'],
+        },
         output: {
-          chunkFileNames: 'assets/chunk-[hash].js',
+          entryFileNames: 'src/pages/[name]/index.js',
+          chunkFileNames: isDev ? 'assets/js/[name].js' : 'assets/js/[name].[hash].js',
+          assetFileNames: (assetInfo) => {
+            const { dir, name: _name } = path.parse(assetInfo.name as string)
+            const assetFolder = dir.split('/').at(-1)
+            const name = assetFolder + firstUpperCase(_name)
+            // TODO: contentStyle
+            return `assets/[ext]/${name}.chunk.[ext]`
+          },
         },
       },
     },
 
     plugins: [
-      crx({ manifest }),
       react(),
+      makeManifest(manifest, {
+        isDev,
+        // contentScriptCssKey: regenerateCacheInvalidationKey(),
+      }),
+      // customDynamicImport(),
       zipPack({
         outDir: `package`,
         inDir: 'build',
@@ -35,3 +66,22 @@ export default defineConfig(({ mode }) => {
     ],
   }
 })
+
+function firstUpperCase(str: string) {
+  const firstAlphabet = new RegExp(/( |^)[a-z]/, 'g')
+  return str.toLowerCase().replace(firstAlphabet, (L) => L.toUpperCase())
+}
+
+/**
+ * Generate content script css key
+ */
+let cacheInvalidationKey: string = generateKey()
+
+function regenerateCacheInvalidationKey() {
+  cacheInvalidationKey = generateKey()
+  return cacheInvalidationKey
+}
+
+function generateKey(): string {
+  return `${(Date.now() / 100).toFixed()}`
+}
