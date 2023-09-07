4be77a1943b7d9b5c067c8e1fde306e7044c3bc0
Hogan-TR <trzerone@gmail.com>
2023-09-07 22:23:56 +0800

    fix: purge vite config
    * format code
    * remove custom build entry - page

--- src/read_pages_folder.ts
+++ src/read_pages_folder.ts
@@ -1,7 +1,8 @@
-import globSync from 'glob';
+import globSync from 'glob'
 
 const pages = await globSync('pages/*.html')
 
-const arrayKeyValuePairs = pages.map(file => [file.split('\\').slice(-1).toString().split('.html').join(''), file])
+const arrayKeyValuePairs = pages.map((file) => [file.split('\\').slice(-1).toString().split('.html').join(''), file])
 
+// like: {'pages/page': 'pages/page.html'}
 export const config = Object.fromEntries(arrayKeyValuePairs)
